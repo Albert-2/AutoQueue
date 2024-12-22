07bba2e6ba33fdedaea8a8fadf70d3c06693b942
Bhushan <alberteinstein02.99792458>
2024-12-22 23:44:02 +0530

    updating vercel file; deploying on vercel

--- server/api/app.js
+++ server/api/app.js
@@ -1,11 +1,9 @@
 import express from "express";
-import bodyParser from "body-parser";
 import connectDB from "./dbConnect.js";
 import queueRoutes from "./routes/queueRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import cors from "cors";
 connectDB();
-
 
 const app = express();
 app.use(express.json());
@@ -15,9 +13,8 @@ app.get("/", (req, res) => {
   res.send("hello world");
 });
 app.listen(5000, () => {
-  console.log("Server is running on port 3001");
+  console.log("Server is running on port 5000");
 });
-
 
 app.use("/api/queues", queueRoutes);
 app.use("/api/users", userRoutes);
